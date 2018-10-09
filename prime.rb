b6aaf1d8f90f835b37c8bbd547de0f3ec231e015
Paolo Ventura <ventura.paolo@gmail.com>
2018-10-09 22:18:07 +0000

    automatically backed up by learn

--- prime.rb
+++ prime.rb
@@ -7,7 +7,7 @@
 #only need to create an array with the multiples and if multiples are 1 and the number then --> prime
 
 def prime?(num)
-   array = (1..num).select { |n|num % n == 0}
+   array = (1..num).select { |n|num % n = 0}
     array.size == 2 ? true : false
  end
  
